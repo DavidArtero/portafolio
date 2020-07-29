dae4071c8a3e4969c58e523eb1f81ad5bfd2482c
Davidama <davidarteromartinez@gmail.com>
2020-07-29 21:54:28 +0200

    controllers/models/routes

--- controllers/project.js
+++ controllers/project.js
@@ -16,8 +16,30 @@ var controller = {
         })
     },
 
+    //Guardar nuevo proyecto
     saveProject:function(req,res){
+        var project = new Project();
 
+        var params = req.body;
+        project.name = params.name;
+        project.description = params.description;
+        project.category = params.category;
+        project.year = params.yeear
+        project.langs = params.langs;
+        project.image = null;
+
+        //Guardar en la base de datos
+        project.save((err,projectStored)=>{
+            if(err) return res.status(500).send({
+                message:'Error al guardar'});
+            
+            if(!projectStored) return res.status(404).send({
+                message: 'No se ha podido guardar el proyecto'});
+
+            return res.status(200).send({
+                project:projectStored
+            });
+        });
     }
 };
 
