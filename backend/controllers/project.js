08ec84e509e3429b013422c7311757cb4a1befb5
Davidama <davidarteromartinez@gmail.com>
2020-07-29 22:52:39 +0200

    estructura proyecto ok

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -19,12 +19,13 @@ var controller = {
     //Guardar nuevo proyecto
     saveProject:function(req,res){
         var project = new Project();
+        message:"Metodo saveProject";
 
         var params = req.body;
         project.name = params.name;
         project.description = params.description;
         project.category = params.category;
-        project.year = params.yeear
+        project.year = params.year;
         project.langs = params.langs;
         project.image = null;
 
@@ -40,7 +41,32 @@ var controller = {
                 project:projectStored
             });
         });
-    }
+    },
+
+    //Devolver un proyecto de la base de datos
+    getProject:function(req,res){
+        var projectId=req.params.id;
+
+        if(projectId==null){
+            return res.status(404).send({message:'El proyecto no existe'});
+        };
+
+        Project.findById(projectId,(err,project)=>{
+            if(err) return res.status(500).send({message:'Error al devolver los datos'});
+            if(!project) return res.status(404).send({message:'El proyecto no existe'});
+            return res.status(200).send({project});
+        });
+    },
+
+    //Devolver todos los proyectos
+    getProjects: function(req,res){
+        Project.find({}).sort('year').exec((err,projects)=>{
+            if(err) return res.status(500).send({message:'Error al devolver los datos'});
+            if(!projects) return res.status(404).send({message:'No hay proyectos para mostrar'});
+            return res.status(200).send({projects});
+        });
+    },
 };
+
 
 module.exports = controller;
